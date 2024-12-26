ece858abe302cea92b9417368a261b2a7ce30ce3
Christian Webber <ricky.webber@hotmail.com>
2024-12-26 13:34:35 -0500

    Removing old templates!

--- Templates/TEMPLATES_BASE_OLD/Velentr.DUAL_SUPPORT/update_fna.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-executable_path = os.path.join(dir_path, "fna_updater", "FnaUpdater.exe")
-
-args = executable_path + " update " + dir_path + " FNA"
-print("Executing: " + args)
-subprocess.call(args, shell=True)
